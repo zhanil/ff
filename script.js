d959a07824bf8b174193b796b3eb0b3ca9de5017
zhanil <125579475+zhanil@users.noreply.github.com>
2024-06-04 12:15:19 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const apiKeyInput = document.querySelector(".apikey");
+const redirectButton = document.querySelector(".redirect");
+
+window.onload = function () {
+  redirectButton.addEventListener("click", (e) => {
+    const apiKey = apiKeyInput.value;
+
+    if (!apiKey.length) return;
+
+    fetch("https://kyc.biometric.kz/api/v1/flows/session/create/", {
+      method: "POST",
+      body: JSON.stringify({
+        api_key: apiKey,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const { session_id, technologies } = data;
+        const url = "https://demo.biometric.kz/flow/" + session_id;
+        window.open(url, "_blank");
+      });
+  });
+};
